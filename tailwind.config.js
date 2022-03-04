716114401b4a072b7ed56cf4843ea9df5123b38e
jtzuya <jtzuya@gmail.com>
2022-03-04 22:40:50 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './config/*.json',
+    './layout/*.liquid',
+    './sections/*.liquid',    
+    './snippets/*.liquid',
+    './templates/*.liquid',
+    './templates/*.json',
+    './templates/customers/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
